eda0acac06d256f6ca3151734fb789991fbd3e8d
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-11-14 01:37:43 -0500

    controller

--- app/Http/Controllers/HeftyBController.php
+++ app/Http/Controllers/HeftyBController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HeftyBController extends Controller
 {
-    //
+    public function index() {
+        return view("landing");
+    }
+
+    public function contact() {
+        return view("contact")
+    }
 }
