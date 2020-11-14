6cb7cbd80f63f89e65d1ece80c3f4266777c0b1b
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-11-14 01:51:00 -0500

    controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use app\Http\Controllers\HeftyBController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
