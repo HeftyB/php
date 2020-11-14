af6df8b0213abf3979c53ded3a59b287a87b580b
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-11-14 01:53:23 -0500

    letter

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use app\Http\Controllers\HeftyBController;
+use App\Http\Controllers\HeftyBController;
 
 /*
 |--------------------------------------------------------------------------
