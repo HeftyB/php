872ec484d41fa4abd0efa1f63f7af49a8818c351
root <ashields0923@gmail.com>
2020-11-14 06:33:03 +0000

    controller

--- app/Http/Controllers/HeftyBController.php
+++ app/Http/Controllers/HeftyBController.php
@@ -1,16 +1,10 @@
 <?php
 
-// namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class HeftyBController extends Controller
 {
-    public function index() {
-        return view('landing');
-    }
-
-    public function contact() {
-        return view('contact');
-    }
+    //
 }
